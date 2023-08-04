4b9c0ce51d597a44bac23c16edb8ddceec8c050f
Jan-Sverre Riksfjord <jansverre@uffe.no>
2023-08-04 09:00:25 +0200

    day four

---
+++ AdventOfCode/Y2022.Tests/DayFour.fs
@@ -0,0 +1,25 @@
+module Y2022.Tests.DayFour
+
+open NUnit.Framework
+open Y2022.DayFour
+
+let testInput = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"
+
+[<Test>]
+let ``Is overlapping works``() =
+    Assert.IsTrue("1-4, 2-3" |> toAssignmentPair |> isTotallyOverlapping)
+    Assert.IsFalse("1-4, 6-8" |> toAssignmentPair |> isTotallyOverlapping)
+    
+[<Test>]
+let ``Count overlapping``() =
+    Assert.AreEqual(2, testInput |> toAssignmentPairs |> countTotallyOverlapping)
+    
+[<Test>]
+let ``Count intersecting``() =
+    Assert.AreEqual(4, testInput |> toAssignmentPairs |> countIntersecting)
---
+++ AdventOfCode/Y2022/DayFour.fs
@@ -0,0 +1,49 @@
+module Y2022.DayFour
+
+open System.Text.RegularExpressions
+
+type Elf = Set<int>
+type AssignmentPair = Elf * Elf
+
+let toRange (sections: string) : Elf =
+    let capture = Regex.Match(sections, "(\d+)-(\d+)")
+    let fromValue = int capture.Groups[1].Value
+    let toValue = int capture.Groups[2].Value
+    Set<int>([ fromValue..toValue ])
+
+let toAssignmentPair input : AssignmentPair =
+    let substrings = input |> Utils.trim |> Utils.split ","
+
+    match substrings with
+    | [| first; second |] -> (first |> toRange, second |> toRange)
+    | _ -> failwith "invalid input format"
+
+let isTotallyOverlapping (elf1, elf2) =
+    let intersect = Set.intersect elf1 elf2
+    (elf1 |> Set.isSuperset intersect) || (elf2 |> Set.isSuperset intersect)
+
+let isIntersecting (elf1, elf2) =
+    not (Set.intersect elf1 elf2 |> Set.isEmpty)
+
+let toAssignmentPairs input =
+    input
+    |> Utils.trim
+    |> Utils.split("\n")
+    |> Seq.map toAssignmentPair
+
+let countTotallyOverlapping pairs =
+    pairs
+    |> Seq.filter(fun p -> p |> isTotallyOverlapping)
+    |> Seq.length
+    
+let countIntersecting pairs =
+    pairs
+    |> Seq.filter(fun p -> p |> isIntersecting)
+    |> Seq.length
+    
+let main() =
+    let input = Utils.readInputFile "04"
+    let assignmentPairs = input |> toAssignmentPairs
+    
+    assignmentPairs |> countTotallyOverlapping |> printfn "%d"
+    assignmentPairs |> countIntersecting |> printfn "%d"
