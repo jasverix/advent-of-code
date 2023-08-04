12a16b5197e75a3a12ac3b3bcfced281e729beb1
Jan-Sverre Riksfjord <jansverre@uffe.no>
2023-08-05 00:12:43 +0200

    day seven part one finished

--- AdventOfCode/Y2022/DaySeven.fs
+++ AdventOfCode/Y2022/DaySeven.fs
@@ -10,11 +10,16 @@ type Folder =
       Files: Map<string, File> }
 
 let createFolder name =
-    { Name = name
-      Folders = Map.empty
-      Files = Map.empty }
+    if name = "" then
+        failwith "empty name given"
+    else
+        { Name = name
+          Folders = Map.empty
+          Files = Map.empty }
 
 let createFile name size = { Name = name; Size = size }
+
+let private _plainRootPath (path: string) = path.Trim().TrimStart('/').TrimEnd('/')
 
 let _addFolderTo (folders: Map<string, Folder>) folder = folders.Add(folder.Name, folder)
 
@@ -24,10 +29,16 @@ let _addSomeFileTo (files: Map<string, File>) (file: File option) =
     else
         files
 
+let _getOrCreateFolder name root =
+    if root.Folders |> Map.containsKey name then
+        root.Folders[name]
+    else
+        createFolder name
+
 let rec _addFolderOn file root (name: string, path: array<string>) =
     { Name = root.Name
       Files = root.Files
-      Folders = _addFolderTo root.Folders (createFolder name |> _addFolder path file) }
+      Folders = _addFolderTo root.Folders (root |> _getOrCreateFolder name |> _addFolder path file) }
 
 and _addFolder (path: array<string>) (file: File option) root =
     if path |> Array.isEmpty then
@@ -44,13 +55,13 @@ let addFolder path root =
     if path = "/" then
         root
     else
-        root |> _addFolder (path.TrimStart('/') |> split "/") None
+        root |> _addFolder (path |> _plainRootPath |> split "/") None
 
 let _addFile (fileName: string, path: array<string>) size root =
     root |> _addFolder path (Some(createFile fileName size))
 
 let addFile path size root =
-    _addFile (path |> split "/" |> popItem) size root
+    _addFile (path |> _plainRootPath |> split "/" |> popItem) size root
 
 type Terminal = { Root: Folder; Path: string }
 
@@ -89,31 +100,40 @@ let handleCommand command terminal =
     | Regex "dir (.+)" [ path ] -> terminal |> indexFolder path
     | Regex "(\d+)\s+(.+)" [ Int size; name ] -> terminal |> indexFile name size
     | _ -> terminal
-    
+
 let handleConsole console terminal =
-    console |> trim |> split "\n"
+    console
+    |> trim
+    |> split "\n"
     |> Seq.fold (fun terminal command -> terminal |> handleCommand command) terminal
-    
+
 let getChildFolder name folder = folder.Folders[name]
 
 let rec getFolderSize folder =
-    let fileSize = folder.Files |> Map.values |> Seq.map(fun f -> f.Size) |> Seq.sum
-    let folderSize = folder.Folders |> Map.values |> Seq.map(fun f -> f |> getFolderSize) |> Seq.sum
+    let fileSize = folder.Files |> Map.values |> Seq.map (fun f -> f.Size) |> Seq.sum
+
+    let folderSize =
+        folder.Folders |> Map.values |> Seq.map (fun f -> f |> getFolderSize) |> Seq.sum
+
     fileSize + folderSize
-    
-let rec getAllFolders root: seq<Folder> =
-    root.Folders |> Map.values
+
+let rec getAllFolders root : seq<Folder> =
+    root.Folders
+    |> Map.values
     |> Seq.map getAllFolders
     |> Seq.concat
     |> Seq.append [ root ]
-    
+
 let sumFoldersLessThan amount folders =
-    folders |> Seq.map getFolderSize |> Seq.filter(fun size -> size < amount) |> Seq.sum
+    folders
+    |> Seq.map getFolderSize
+    |> Seq.filter (fun size -> size < amount)
+    |> Seq.sum
 
 let initiateTerminal () = { Root = createFolder "/"; Path = "/" }
 
-let main() =
+let main () =
     let input = readInputFile "07"
-    let terminal = initiateTerminal() |> handleConsole input
-    
-    terminal.Root |> getAllFolders |> sumFoldersLessThan 10000 |> printfn "%d"
+    let terminal = initiateTerminal () |> handleConsole input
+
+    terminal.Root |> getAllFolders |> sumFoldersLessThan 100000 |> printfn "%d"
