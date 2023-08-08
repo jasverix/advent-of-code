1b6fdb87e2f8f0d33aa4e04a3c247eff106988dc
Jan-Sverre Riksfjord <jansverre@uffe.no>
2023-08-08 21:46:30 +0200

    day 12 - heavily inspired by Aksel

--- AdventOfCode/Y2022.Tests/Day12.fs
+++ AdventOfCode/Y2022.Tests/Day12.fs
@@ -12,48 +12,15 @@ abdefghi
 "
 
 [<Test>]
-let ``Can go to`` () = Assert.IsTrue('d' |> canGoto 'e')
-
-[<Test>]
-let ``Next step from (4,2)`` () =
-    let map = testInput |> toMap
-    let spot = map |> getSpot (4, 2) |> Option.get
-
-    Assert.AreEqual('d', spot.Elevation)
-
-    let nextSteps = spot |> nextSteps []
-
-    Assert.AreEqual(
-        [ [ (3, 2) ];  [ (4, 3) ]; [ (4, 1) ] ] |> List.map string |> String.concat " ",
-        nextSteps |> List.map string |> String.concat " "
-    )
-    
-[<Test>]
-let ``Next step from (0,6)`` () =
-    let map = testInput |> toMap
-    let spot = map |> getSpot (0, 6) |> Option.get
-
-    Assert.AreEqual('n', spot.Elevation)
-
-    let nextSteps = spot |> nextSteps []
-
-    Assert.AreEqual(
-        [ [ (0, 7) ];  [ (0, 5) ] ] |> List.map string |> String.concat " ",
-        nextSteps |> List.map string |> String.concat " "
-    )
-
-[<Test>]
-let ``Next step of viable path``() =
-    let toString list = list |> List.map string |> String.concat " "
-    let viablePath = [ (0,0); (1,0); (1,1); (2,1); (2,2); (3,2); (4,2); (4,3); (4,4); (4,5); (4,6) ] |> List.rev
-    let map = testInput |> toMap
-    let nextSteps = nextStepsOrSelf viablePath (map |> getSpot (4,6) |> Option.get) (map |> getEnd).Pos
-    Assert.AreEqual([ (4,7) :: viablePath ] |> toString, nextSteps |> toString)
-
-[<Test>]
 let ``Shortest path to goal`` () =
     let map = testInput |> toMap
-    let paths = pathsToEnd map
-    let shortest = paths |> List.map List.length |> List.min
-
+    let distanceMap = map |> findDistanceMapDownwards (map |> getEnd)
+    let shortest = distanceMap |> findShortestPathToGoal (map |> getStart)
     Assert.AreEqual(31, shortest)
+
+[<Test>]
+let ``Shortest path to bottom`` () =
+    let map = testInput |> toMap
+    let distanceMap = map |> findDistanceMapDownwards (map |> getEnd)
+    let shortest = distanceMap |> findShortestPathToBottom map
+    Assert.AreEqual(29, shortest)
--- AdventOfCode/Y2022/Day12.fs
+++ AdventOfCode/Y2022/Day12.fs
@@ -5,143 +5,22 @@ open Utils
 type Elevation = char
 type Position = int * int
 type HeightMap = Map<Position, Elevation>
+type Distance = int
+type DistanceMap = Map<Position, Distance>
 
-type Spot =
-    { Pos: Position
-      Map: HeightMap
-      Elevation: Elevation }
-
-let getElevation char =
+let parseElevation char =
     match char with
-    | 'S' -> 'a'
-    | 'E' -> 'z'
-    | c -> c
+    | 'S' -> 0
+    | 'E' -> int 'z' - (int 'a')
+    | c -> int c - int 'a'
 
-let getSpot pos map =
-    if map |> Map.containsKey pos then
-        Some
-            { Pos = pos
-              Map = map
-              Elevation = getElevation map[pos] }
-    else
-        None
+let getStart (map: HeightMap) = map |> Map.findKey (fun _ s -> s = 'S')
+let getEnd (map: HeightMap) = map |> Map.findKey (fun _ s -> s = 'E')
 
-let getSpot2 map pos = getSpot pos map
+let canComeFrom (target: Elevation) (source: Elevation) =
+    parseElevation target >= (parseElevation source - 1)
 
-let getStart (map: HeightMap) =
-    map |> Map.findKey (fun _ s -> s = 'S') |> getSpot2 map |> Option.get
-
-let getEnd (map: HeightMap) =
-    map |> Map.findKey (fun _ s -> s = 'E') |> getSpot2 map |> Option.get
-
-let canGoto (target: Elevation) (source: Elevation) = int target <= (int source + 1)
-
-type Direction =
-    | Up
-    | Left
-    | Right
-    | Down
-
-type Path = Position list
-
-let step dir (lat, lng) =
-    match dir with
-    | Up -> (lat - 1, lng)
-    | Down -> (lat + 1, lng)
-    | Left -> (lat, lng - 1)
-    | Right -> (lat, lng + 1)
-
-let stepSpot dir spot =
-    spot.Pos |> step dir |> getSpot2 spot.Map
-
-let _getSurroundingSpots pos map =
-    [ getSpot (pos |> step Down) map
-      getSpot (pos |> step Up) map
-      getSpot (pos |> step Right) map
-      getSpot (pos |> step Left) map ]
-    |> List.filter Option.isSome
-    |> List.map Option.get
-
-let getSurroundingSpots spot = _getSurroundingSpots spot.Pos spot.Map
-
-let getAccessibleSpots spot =
-    spot
-    |> getSurroundingSpots
-    |> List.filter (fun s -> spot.Elevation |> canGoto s.Elevation)
-
-let walk dir (start: Spot) =
-    start
-    |> stepSpot dir
-    |> Seq.unfold (fun spot ->
-        if spot.IsSome then
-            let nextSpot = spot.Value |> stepSpot dir
-
-            if nextSpot.IsSome then
-                if spot.Value.Elevation |> canGoto nextSpot.Value.Elevation then
-                    Some(spot, nextSpot)
-                else
-                    None
-            else
-                None
-        else
-            None)
-    |> Seq.takeWhile Option.isSome
-    |> Seq.map Option.get
-
-let nextSteps (currentPath: Path) (currentSpot: Spot) : Path list =
-    currentSpot
-    |> getAccessibleSpots
-    |> List.filter (fun s -> not (List.contains s.Pos currentPath))
-    |> List.map (fun s -> s.Pos :: currentPath)
-
-let nextStepsOrSelf currentPath currentSpot target : Path list =
-    let next = nextSteps currentPath currentSpot
-
-    if next.Length = 0 then
-        if currentPath |> List.contains target then
-            [ currentPath ]
-        else
-            []
-    else
-        next
-
-let getNextSteps map (paths: Path list) target =
-    paths
-    |> List.map (fun path -> nextStepsOrSelf path (getSpot path.Head map).Value target)
-    |> List.concat
-
-let getShortestPath (paths: Path list) (pos: Position) =
-    let findIndex (index: int option) =
-        match index with
-        | Some index -> index
-        | None -> -1
-
-    let reverse index = (paths.Length - index)
-
-    paths
-    |> List.minBy (fun path -> path |> List.tryFindIndexBack (fun p -> p = pos) |> findIndex |> reverse)
-
-let onlyShortestPaths (paths: Path list) =
-    paths
-    |> List.map List.head
-    |> List.map (getShortestPath paths)
-    |> Set.ofList
-    |> Set.toList
-
-let allPathsToTarget target spot =
-    let mutable paths = getNextSteps spot.Map [ [ spot.Pos ] ] target
-    let withTarget p = p |> List.contains target
-
-    while paths.Length <> (paths |> List.filter withTarget).Length do
-        paths <- getNextSteps spot.Map paths target |> onlyShortestPaths
-
-    paths |> List.filter (fun p -> p |> List.contains target) |> List.map List.tail
-
-
-let containsSpot spot (path: Path) = path |> List.contains spot.Pos
-
-let pathsToEnd map =
-    map |> getStart |> allPathsToTarget (map |> getEnd |> (fun s -> s.Pos))
+let parseRow (row: string) = row |> Seq.toArray
 
 let private _toRow (lat: int) (row: string) : (Position * Elevation) array =
     row |> Array.ofSeq |> Array.mapi (fun lng char -> ((lat, lng), char))
@@ -149,6 +28,54 @@ let private _toRow (lat: int) (row: string) : (Position * Elevation) array =
 let toMap input : HeightMap =
     input |> trim |> split "\n" |> Array.mapi _toRow |> Array.concat |> Map.ofArray
 
-let main () =
+let findNeighbours (map: HeightMap) (pos: Position) : Position list =
+    let add (a1, a2) (b1, b2) = a1 + b1, a2 + b2
+
+    match map |> Map.tryFind pos with
+    | None -> []
+    | Some height ->
+        [ (0, 1); (1, 0); (0, -1); (-1, 0) ]
+        |> Seq.map (add pos)
+        |> Seq.choose (fun neighbour ->
+            map
+            |> Map.tryFind neighbour
+            |> Option.filter (fun v -> height |> canComeFrom v)
+            |> Option.map (fun _ -> neighbour))
+        |> Seq.toList
+
+let findDistanceMapDownwards (origin: Position) terrain : DistanceMap =
+    let rec propagateDistance distances positions =
+        match positions with
+        | [] -> distances
+        | (pos, distance) :: xs when Map.tryFind pos distances |> Option.exists (fun v -> v <= distance) ->
+            propagateDistance distances xs
+        | (pos, distance) :: xs ->
+            let newDistanceMap = distances |> Map.add pos distance
+
+            let newNeighbours =
+                findNeighbours terrain pos |> List.map (fun p -> (p, distance + 1))
+
+            let neighborsToPropagate =
+                newNeighbours
+                |> Seq.filter (fun (pos, dist) ->
+                    newDistanceMap |> Map.containsKey pos |> not
+                    || newDistanceMap |> Map.tryFind pos |> Option.exists (fun v -> v > dist))
+                |> Seq.toList
+
+            propagateDistance newDistanceMap (neighborsToPropagate @ xs)
+
+    propagateDistance Map.empty [ (origin, 0) ]
+
+let findShortestPathToGoal goal distanceMap =
+    distanceMap |> Map.find goal
+    
+let findShortestPathToBottom map distanceMap =
+    let zeroLevels = map |> Map.filter (fun k v -> v = 'a') |> Map.keys
+    distanceMap |> Map.filter (fun k _ -> zeroLevels.Contains(k)) |> Map.values |> Seq.min    
+    
+let main() =
     let map = readInputFile "12" |> toMap
-    map |> pathsToEnd |> List.map List.length |> List.min |> printfn "%d"
+    let distanceMap = map |> findDistanceMapDownwards (map |> getEnd)
+    let start = map |> getStart
+    distanceMap |> findShortestPathToGoal start |> printfn "%d"
+    distanceMap |> findShortestPathToBottom map |> printfn "%d"
